b378a8a8774118c4b62b71af1b0288bc3ce70b83
Suellen Cândido <88504054+suellen-candido@users.noreply.github.com>
2026-01-20 15:05:27 -0300

    Adicionando mais especies.py

--- especies.py
+++ especies.py
@@ -1,4 +1,16 @@
-from plantas import PlantaVascular
+from plantas import PlantaVascular, PlantaAvascular
+
+class Briofita(PlantaAvascular):
+    def exibir_classificacao(self):
+        return f"{self.get_nome()} | Categoria: AVASCULAR (Briófita)"
+
+class Pteridofita(PlantaVascular):
+    def exibir_classificacao(self):
+        return f"{self.get_nome()} | Categoria: VASCULAR -> SEM SEMENTE (Pteridófita)"
+
+class Gimnosperma(PlantaVascular):
+    def exibir_classificacao(self):
+        return f"{self.get_nome()} | Categoria: VASCULAR -> COM SEMENTE -> SEM FRUTO (Gimnosperma)"
 
 class Angiosperma(PlantaVascular):
     def exibir_classificacao(self):
